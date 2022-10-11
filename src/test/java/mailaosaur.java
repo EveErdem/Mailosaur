2aefd897783a3b3321939a1834784266b74835f2
EceNarin <la.crymosa321@gmail.com>
2022-10-11 17:21:10 +0300

    first commit

---
+++ src/test/java/mailaosaur.java
@@ -0,0 +1,12 @@
+import com.mailosaur.MailosaurClient;
+import com.mailosaur.MailosaurException;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class mailaosaur {
+public static void sendmail(String recepient){
+
+
+}
+}
