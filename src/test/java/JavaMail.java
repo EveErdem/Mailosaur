398731e57b2f444f706c42eecc3c33518a405e48
EceNarin <la.crymosa321@gmail.com>
2022-11-14 22:08:43 +0300

    add

--- src/test/java/JavaMail.java
+++ src/test/java/JavaMail.java
@@ -1,13 +1,1 @@
-import org.junit.jupiter.api.Test;
-
-import javax.mail.MessagingException;
-
-public class JavaMail {
-    public static void main(String[] args) throws MessagingException {
-        JavaMailUtil.sendmail("la.crymosa321@gmail.com");
-    }
-    @Test
-    void name() throws MessagingException {
-        JavaMailUtil.sendmail("la.crymosa321@gmail.com");
-    }
-}
+z
